a5583161834e67426712a48755f83620c362b19d
aalniak <ardaalniak2001@gmail.com>
2025-03-16 14:23:53 +0300

    Add required installations

--- test_kitti.sh
+++ test_kitti.sh
@@ -1,3 +1,4 @@
+pip install kagglehub
 source ~/anaconda3/etc/profile.d/conda.sh
 conda activate depth-pro
 python test_kitti.py
--- test_nyu.sh
+++ test_nyu.sh
@@ -1,3 +1,4 @@
+pip install  huggingface
 source ~/anaconda3/etc/profile.d/conda.sh
 conda activate depth-pro
 python test_nyu.py
